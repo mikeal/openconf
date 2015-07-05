c6404009cb6c9ea4679c6b7f3c7e5be2e44089c0
Mikeal Rogers <mikeal.rogers@gmail.com>
2015-07-04 21:05:49 -0700

    Working conferencing.

--- index.js
+++ index.js
@@ -6,39 +6,131 @@ var http = require('http')
   , level = require('level')
   , url = require('url')
   , sublevel = require('level-sublevel')
-  , indexer = require('level-sec')
+  , index = require('level-index')
   , eachy = require('eachy')
-
+  , uuid = require('node-uuid')
+  , flatten = require('flatten')
+  , phone = require('phone')
   , qs = require('querystring')
   , request = require('request').defaults({headers:{'user-agent':'openconf-0.0.1'}})
+  , router = require('http-hash-router')()
 
   , db = level(path.join(__dirname, 'db'), {valueEncoding:'json'})
   , levelSession = require('level-session')({db:db})
 
   , host = 'conf.tenconf.com'
-
   , urlbase = 'http://conf.tenconf.com/'
+  , config =
+    { accountSid: process.env.TWILIO_SID
+    , authToken: process.env.TWILIO_TOKEN
+    , twilioNumber: '+12017318489'
+    , port: process.env.PORT || 80
+    }
+  , client = require('twilio')(config.accountSid, config.authToken)
+  ;
 
-  , config = {
-    // Twilio Account SID - found on your dashboard
-    accountSid: process.env.TWILIO_SID,
+var tokens = sublevel(db).sublevel('tokens')
+  , confs = sublevel(db).sublevel('confs')
+  , contacts = sublevel(db).sublevel('contacts')
+  ;
 
-    // Twilio Auth Token - found on your dashboard
-    authToken: process.env.TWILIO_TOKEN,
+function reindex () {
+  confs.createReadStream().on('data', function (doc) {
+    confs.put(doc.id, doc, console.log)
+  })
+}
 
-    // A Twilio number that you have purchased through the twilio.com web
-    // interface or API
-    twilioNumber: '+12017318489',
+var byOrg = index(confs,
+    'byOrg',
+    function (key, value, emit) {
+      var obj = value;
+      if (!obj.org) return
+      emit(obj.org, { id : key, })
+    }
+)
 
-    // The port your web application will run on
-    port: process.env.PORT || 80
-  }
+function docsByKeys (db, index, keys, cb) {
+  eachy(
+  keys,
+  function (key, cb) {
+    var ids = []
+    index.createReadStream({start:key, end: key})
+    .on('data', function (obj) {
+      ids.push(obj.value.id)
+    })
+    .on('end', function () {
+      cb(null, ids)
+    })
+  },
+  function (err, ids) {
+    ids = flatten(ids)
+    function _get (id, cb) {
+      db.get(id, function (err, obj) {
+        if (err) return cb(null)
+        cb(err, obj)
+      })
+    }
+    eachy(ids, _get, cb)
+  })
+}
 
-var router = require('http-hash-router')()
+function scheduler (dt, obj, cb) {
+  var now = Date.now()
+    , future = dt.getTime()
+    ;
+  return setTimeout(function () {cb(obj)}, future - now)
+}
+
+var timeouts = {}
+function callConference (id) {
+  confs.get(id, function (err, doc) {
+    if (err || !doc.rsvp) return console.error('issue w/ doc', id)
+    eachy(
+    doc.rsvp,
+    function (user, cb) {
+      contacts.get(user.login, function (err, contact) {
+        if (err) return cb(null)
+        cb(null, contact)
+      })
+    },
+    function (err, contacts) {
+      if (err) return console.error('unresolvable error w/', id)
+      contacts = contacts.filter(function (c) {return c})
+      contacts.push('+14159925092')
+      contacts.forEach(function (contact) {
+        console.log('calling', contact)
+        var url = 'http://conf.tenconf.com/conf/'+doc.id
+        client.makeCall(
+          { to: contact
+          , from: config.twilioNumber
+          , url: url
+          },
+        function(err, message) {
+          console.log('call', err, message)
+        })
+      })
+    })
+  })
+}
+function onConference (conf) {
+  var conf = conf.value
+  var dt = new Date(conf.dt)
+  if (dt.getTime() < Date.now()) return
+  if (timeouts[conf.id]) clearTimeout(timeouts[conf.id])
+  timeouts[conf.id] = scheduler(dt, conf.id, callConference)
+}
+confs.createReadStream()
+.on('data', onConference)
+.on('end', function () {
+  confs.post(function (ch) {
+    onConference(ch)
+  })
+})
+
+callConference('57579f10-22c5-11e5-a3c7-6feea46ce6bd')
 
 router.set('/', function (req, res) {
   levelSession(req, res, function () {
-    console.log(req.session.token)
     var f = fs.createReadStream(path.join(__dirname, 'index.html'))
     return f.pipe(response.html()).pipe(res)
   })
@@ -61,16 +153,38 @@ router.set('/call', function (req, res) {
   })
 })
 
-var tokens = sublevel(db).sublevel('tokens')
-  , confs = sublevel(db).sublevel('confs')
-  , indexes = indexer(confs)
-    .by('org')
-    .by('owner')
-    .db
-  ;
+function conferenceTemplate (doc) {
+  var ret =
+  '<Response>' +
+    '<Say>Joining conference now.</Say>' +
+    '<Dial>' +
+        '<Conference ' +
+        'record="record-from-start" ' +
+        'eventCallbackUrl="http://conf.tenconf.com/conf-end/'+doc.id+'">'+doc.id+'</Conference>' +
+    '</Dial>' +
+  '</Response>'
+  return ret
+}
 
-confs.createReadStream().on('data', function (obj) {
-  console.log(obj)
+router.set('/conf/:id', function (req, res, opts) {
+  body(req, function (err, data) {
+    confs.get(opts.params.id, function (err, doc) {
+      console.log('confsdb', err, doc)
+      if (err) return response.error(404).pipe(res)
+      console.log(conferenceTemplate(doc))
+      response.xml(conferenceTemplate(doc)).pipe(res)
+    })
+  })
+})
+router.set('/conf-end/:id', function (req, res, opts) {
+  body(req, function (err, data) {
+    console.log(data)
+    if (err) return response.error(err).pipe(res)
+    confs.get(opts.params.id, function (err, doc) {
+      if (err) return response.error(404).pipe(res)
+      // TODO - write to soundcloud
+    })
+  })
 })
 
 router.set('/outbound', function (req, res) {
@@ -83,7 +197,6 @@ router.set('/outbound', function (req, res) {
   '</Response>'
   return response.xml(xml).pipe(res)
 })
-
 router.set('/intl-tel-input/*', function (req, res) {
   var f = req.url.slice('/intl-tel-input'.length)
     , p = path.join(__dirname, 'node_modules', 'intl-tel-input', 'build', f)
@@ -112,7 +225,6 @@ router.set('/callback', function (req, res) {
         }
       , u = 'https://github.com/login/oauth/access_token?'+qs.stringify(opts)
       ;
-    console.log(opts)
     request(u, function (e, resp, body) {
       if (e) return response.error(e).pipe(res)
       if (resp.statusCode !== 200) {
@@ -121,13 +233,11 @@ router.set('/callback', function (req, res) {
       var token = qs.parse(body)
         , opts = {json:true}
         ;
-      console.log(opts)
       request('https://api.github.com/user?access_token='+token.access_token, opts, function (e, r, body) {
         if (e) return response.error(e).pipe(res)
         if (r.statusCode !== 200) {
           return response.error(new Error('statusCode not 200, '+resp.statusCode)).pipe(res)
         }
-        console.log(body)
         token.user = body
         req.session.set('token', token, function (err) {
           if (err) return response.error(err).pipe(res)
@@ -147,29 +257,93 @@ router.set('/list', function (req, res) {
       if (!body) {
         return response.json({error:'not logged in'}).pipe(res)
       }
-      var u = 'https://api.github.com/user/orgs?access_token='+body.access_token
-      request(u, {json:true}, function (err, r, body) {
-        if (err) return response.error(err).pipe(res)
-        if (r.statusCode !== 200) {
-          return response.error(new Error('statusCode not 200, '+r.statusCode)).pipe(res)
-        }
-        var orgs = body.map(function (obj) {return obj.login})
-        var count = 0
+      var user = body.user
 
-        orgs.forEach(function (org) {
-          var confs = []
-          indexes.byorg.get(org, function (err, _confs) {
-            if (!err) confs = confs.concat(_confs)
-            count = count + 1
-            if (count === orgs.length) {
-              response.json({confs:confs, orgs:orgs}).pipe(res)
-            }
+      contacts.get(user.login, function (err, phone) {
+        var u = 'https://api.github.com/user/orgs?access_token='+body.access_token
+        request(u, {json:true}, function (err, r, body) {
+          if (err) return response.error(err).pipe(res)
+          if (r.statusCode !== 200) {
+            return response.error(new Error('statusCode not 200, '+r.statusCode)).pipe(res)
+          }
+          var orgs = body.map(function (obj) {return obj.login})
+          var count = 0
+
+          docsByKeys(confs, byOrg, orgs, function (err, _confs) {
+            response.json({confs:_confs, orgs:orgs, phone:phone, user:user}).pipe(res)
+          })
+
+        })
+      })
+    })
+  })
+})
+router.set('/create', function (req, res) {
+  body(req, function (err, data) {
+    if (err) return response.error(err).pipe(res)
+    levelSession(req, res, function () {
+      req.session.get('token', function (err, body) {
+        if (err) return response.error(err).pipe(res)
+        if (!body) {
+          return response.json({error:'not logged in'}).pipe(res)
+        }
+        data.user = body.user
+        data.id = uuid.v1()
+        confs.put(data.id, data, function (err) {
+          if (err) return response.error(err).pipe(res)
+          return response.json(data).pipe(res)
+        })
+      })
+    })
+  })
+})
+router.set('/rsvp', function (req, res) {
+  body(req, function (err, data) {
+    if (err) return response.error(err).pipe(res)
+    levelSession(req, res, function () {
+      req.session.get('token', function (err, token) {
+        if (err) return response.error(err).pipe(res)
+        if (!token) {
+          return response.json({error:'not logged in'}).pipe(res)
+        }
+        confs.get(data.id, function (err, doc) {
+          if (!doc.rsvp) doc.rsvp = []
+          // console.log(token.user)
+          // console.log(doc.rsvp)
+          for (var i=0;i<doc.rsvp.length;i++) {
+            if (token.user.login === doc.rsvp[i].login) return response.json({error:"Already RSVP'd."}).pipe(res)
+          }
+          doc.rsvp.push(token.user)
+
+          confs.put(doc.id, doc, function (err, info) {
+            if (err) return response.error(err).pipe(res)
+            response.json(info).pipe(res)
           })
         })
       })
     })
   })
 })
+router.set('/set-contact', function (req, res) {
+  body(req, function (err, data) {
+    if (err) return response.error(err).pipe(res)
+
+    var num = phone(data)
+    if (!num[0]) return response.json({error:'number is not valid.'}).pipe(res)
+
+    levelSession(req, res, function () {
+      req.session.get('token', function (err, token) {
+        if (err) return response.error(err).pipe(res)
+        contacts.put(token.user.login, num[0], function (err, info) {
+          if (err) return response.error(err).pipe(res)
+          res.statusCode = 201
+          res.end()
+        })
+      })
+    })
+  })
+})
+
 router.set('/signout', function (req, res) {
   levelSession(req, res, function () {
     req.session.delAll(function () {
@@ -188,6 +362,7 @@ router.set('/static/*', function (req, res) {
 })
 
 http.createServer(function (req, res) {
+  console.log(req.method, req.url)
   router(req, res, {}, function (err) {
     return response.error(err).pipe(res)
   })
